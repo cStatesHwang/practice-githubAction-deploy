cab16d39046fe3da84554570ff2e3ded59c9f353
cStatesHwang <jjmes1992@gmail.com>
2023-02-06 14:04:52 +0900

    lll

---
+++ src/test/java/com/codetates/practicegithubActiondeploy/PracticeGithubActionDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codetates.practicegithubActiondeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithubActionDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
